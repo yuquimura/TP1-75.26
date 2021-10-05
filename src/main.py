920f681914185e3ea18f87f002cb89adb6c1397f
brian <brianzambellitello@gmail.com>
2021-10-05 19:00:47 -0300

    Fix de # de iteraciones

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from GLC import GLC
 def main():
     glc = GLC(9, 4, 0, 1)
     # Primer punto
-    # glc.create_plot(1587)
+    glc.create_plot(100000)
     # Segundo punto
     # glc.create_plot(10000)
     # glc.create_plot(1000)
     # glc.create_plot(100)
-    glc_2 = GLC(79, 1, 1, 1)
-    glc_2.create_plot(10)
-    # glc_2.create_plot(1000)
+    # glc_2 = GLC(79, 1, 1, 1)
+    # glc_2.create_plot(10)
+    # # glc_2.create_plot(1000)
     # glc_2.create_plot(100)
 
 
