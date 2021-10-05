b240ff4eb405cb945eba61ab0536cfe84e1213e9
brian <brianzambellitello@gmail.com>
2021-10-05 18:56:28 -0300

    New: Implementacion de los metodos y clases para el segundo punto

--- src/main.py
+++ src/main.py
@@ -6,13 +6,13 @@ def main():
     # Primer punto
     # glc.create_plot(1587)
     # Segundo punto
-    glc.create_plot(10000)
-    glc.create_plot(1000)
-    glc.create_plot(100)
-    glc_2 = GLC(9, 1, 1, 1)
-    glc_2.create_plot(10000)
-    glc_2.create_plot(1000)
-    glc_2.create_plot(100)
+    # glc.create_plot(10000)
+    # glc.create_plot(1000)
+    # glc.create_plot(100)
+    glc_2 = GLC(79, 1, 1, 1)
+    glc_2.create_plot(10)
+    # glc_2.create_plot(1000)
+    # glc_2.create_plot(100)
 
 
 main()
